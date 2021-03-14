e4fba11e10113c8abf56944ea50d268f447c83a3
ogamw <hajime.ogasawara1201@gmail.com>
2021-03-15 03:58:19 +0900

    メモ編集画面とメモ作成画面の作成

--- App.jsx
+++ App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import メモリスト画面 from './src/screens/メモリスト画面';
 import メモ詳細画面 from './src/screens/メモ詳細画面';
+import メモ編集画面 from './src/screens/メモ編集画面';
+import メモ作成画面 from './src/screens/メモ作成画面';
 
 export default function App() {
   return (
     // eslint-disable-next-line react/jsx-pascal-case
-    <メモリスト画面 />
+    <メモ作成画面 />
   );
 }
