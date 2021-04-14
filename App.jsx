7546e8632d73af3593e2419f1c1a9f778d0935a9
ogamw <hajime.ogasawara1201@gmail.com>
2021-04-15 02:53:18 +0900

    サインアップ画面の作成

--- App.jsx
+++ App.jsx
@@ -4,10 +4,11 @@ import React from 'react';
 // import メモ詳細画面 from './src/screens/メモ詳細画面';
 // import メモ編集画面 from './src/screens/メモ編集画面';
 // import メモ作成画面 from './src/screens/メモ作成画面';
-import ログイン画面 from './src/screens/ログイン画面';
+// import ログイン画面 from './src/screens/ログイン画面';
+import サインアップ画面 from './src/screens/サインアップ画面';
 
 export default function App() {
   return (
-    <ログイン画面 />
+    <サインアップ画面 />
   );
 }
