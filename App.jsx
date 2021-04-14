5963fdda3f487853ac6e48fd6a16ddeaf8907df6
ogamw <hajime.ogasawara1201@gmail.com>
2021-04-15 02:48:41 +0900

    ログイン画面の作成

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import メモリスト画面 from './src/screens/メモリスト画面';
-import メモ詳細画面 from './src/screens/メモ詳細画面';
-import メモ編集画面 from './src/screens/メモ編集画面';
-import メモ作成画面 from './src/screens/メモ作成画面';
+// import メモリスト画面 from './src/screens/メモリスト画面';
+// import メモ詳細画面 from './src/screens/メモ詳細画面';
+// import メモ編集画面 from './src/screens/メモ編集画面';
+// import メモ作成画面 from './src/screens/メモ作成画面';
+import ログイン画面 from './src/screens/ログイン画面';
 
 export default function App() {
   return (
-    // eslint-disable-next-line react/jsx-pascal-case
-    <メモ作成画面 />
+    <ログイン画面 />
   );
 }
