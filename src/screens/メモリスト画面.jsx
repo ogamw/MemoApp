9d26bb299e393edb719bc7ab08fb073e2c519f8f
ogamw <hajime.ogasawara1201@gmail.com>
2021-06-26 18:16:45 +0900

    ローディング状態の表示を実装

--- src/screens/メモリスト画面.jsx
+++ src/screens/メモリスト画面.jsx
@@ -8,10 +8,12 @@ import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogOutButton from '../components/LogOutButton';
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function メモリスト画面(props) {
   const { navigation } = props;
   const [memos, setMemos] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogOutButton />,
@@ -23,6 +25,7 @@ export default function メモリスト画面(props) {
     const { currentUser } = firebase.auth();
     let unsubscribe = () => {};
     if (currentUser) {
+      setLoading(true);
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
       unsubscribe = ref.onSnapshot((snapshot) => {
         const userMemos = [];
@@ -36,8 +39,10 @@ export default function メモリスト画面(props) {
           });
         });
         setMemos(userMemos);
+        setLoading(false);
       }, (error) => {
         console.log(error);
+        setLoading(false);
         Alert.alert('データの読み込みに失敗しました。');
       });
     }
@@ -47,6 +52,7 @@ export default function メモリスト画面(props) {
   if (memos.length === 0) {
     return (
       <View style={emptyStyles.container}>
+        <Loading isLoading={isLoading} />
         <View style={emptyStyles.inner}>
           <Text style={emptyStyles.title}>最初のメモを作成しよう！</Text>
           <Button
